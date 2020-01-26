55685464efd362621094b2fd79fdf513448b2822
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-01-26 17:18:23 +0300

    add test infrastructure

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+import path from 'path';
+import pluginTester from 'babel-plugin-tester';
+import styledPropsConditions from '../src';
+
+pluginTester({
+  plugin: styledPropsConditions,
+  fixtures: path.join(__dirname, 'fixtures'),
+});
